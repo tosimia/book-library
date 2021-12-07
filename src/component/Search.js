fe0fd38d8d63e918efc8c09920dffeb2c8a93de2
tosimiadesoye <tosimiadesoye111@gmail.com>
2021-12-07 17:36:29 +0000

    added switch statement to search

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import {  getBook} from "../../services";
 const Search = () => {
+     //the query for search api is key word
+  const [keyword, setKeyword] = useState("");
+    const searchBy = async () =>{
+        let value = ""
+        switch (value){
+            case "intitle":
+                break;
+            case "inauthor":
+                break;
+            case "subject":
+                break;
+                case "ispublisher":
+                    break;
+        }
+        const response = await getBook();
+        if (response){
+            setBook(response.data.items)
+        }
+    }
     return(
         <div>
 
