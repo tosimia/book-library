ffa4c445bd7fdabf777208b920b684b0839a167e
tosimiadesoye <tosimiadesoye111@gmail.com>
2021-12-10 10:27:57 +0000

    added model

--- src/component/DisplayOnlyOneItem.js
+++ src/component/DisplayOnlyOneItem.js
@@ -1,8 +1,9 @@
 import { useLocation} from "react-router";
 import { BookContext } from "../utils/store";
-import { useContext } from "react";
+import { useContext,useState} from "react";
 import Navigation from "./Navigation";
 const DisplayOnlyOneItem = () =>{
+    const [showMore, setShowMore] = useState(false);
     const value = useContext(BookContext)
 
     let location = useLocation();
@@ -13,6 +14,38 @@ const DisplayOnlyOneItem = () =>{
         } else {
             info = location.state.data;
         }
+console.log(value.book.categories)
+    const getText = () =>{
+        const text = info.volumeInfo.description 
+        if(!text) return ""
+        //if text is shorter than desired length
+        if(text.length <= 258) return <p>{text}</p>;
+
+        //if text is longer than desired length & showMore is true
+        if(text > 258 && showMore){
+            return(
+                <>
+                    <p>{text}</p>
+                    <button
+                        onClick={() => setShowMore(false)}>
+                        Show less
+                    </button>
+                </>
+            );
+        };
+
+        // if text is longer than desired length & showMore is false
+        if(text.length > 258){
+            return(
+                <>
+                <p>{text.slice(0,258)}</p>
+                <button onClick={()=> setShowMore(true)}>
+                    Show full description
+                </button>
+                </>
+            )
+        }
+    }
 
     return(
         <div>
@@ -21,7 +54,10 @@ const DisplayOnlyOneItem = () =>{
             <img src={info.volumeInfo.imageLinks.smallThumbnail}/>
                 <h2>{info.volumeInfo.title}</h2>
                  <h3>{value.Author(info) }</h3>  
-                 <p>{info.volumeInfo.description}</p>
+                 {/* <p>{info.volumeInfo.description}</p>  */}
+                 <div>{getText()}</div>
+                 {/* <p>{info.searchInfo.textSnippet}</p> */}
+                 <p>{info.categories}</p>
                 <a href={info.accessInfo.pdf.acsTokenLink}>Download pdf</a>
                 <a href={info.volumeInfo.infoLink}>Google link</a> 
                 <button 
